2fa5c1d45dff62f4ae16bb30f86056de937c449c
Benjamin Beilharz <b@beilharz.io>
2021-11-30 17:08:01 +0100

    Added further preprocessing and tested pretraining files

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,8 +1,18 @@
+"""
+Dataset preprocessing tool
+Run from root.
+"""
+
 import re
 from glob import glob
+from functools import partial
+from pprint import pprint
 from typing import Tuple, List, Mapping
 
 import pandas as pd
+from reader import read_jsonlines
+
+glob = partial(glob, recursive=True)
 
 
 def rainbow_data(glob_path: str = './data/rainbow/**/train*') -> pd.DataFrame:
@@ -32,7 +42,7 @@ def rainbow_data(glob_path: str = './data/rainbow/**/train*') -> pd.DataFrame:
 
 
 def social_chem(glob_path: str = './data/processed/social_chem*') -> pd.DataFrame:
-    datasets = glob(glob_path, recursive=True)
+    datasets = glob(glob_path)
     cols = ['split', 'rot-categorization', 'rot-judgment', 'action', 'action-agree', 'situation', 'rot']
     
     df = pd.read_csv(datasets[0], sep='\t', encoding='utf8')[cols]
@@ -43,7 +53,7 @@ def social_chem(glob_path: str = './data/processed/social_chem*') -> pd.DataFram
 
 
 def scruples(glob_path: str = './data/processed/*scruples*') -> Tuple[pd.DataFrame, pd.DataFrame]:
-    datasets = glob(glob_path, recursive=True)
+    datasets = glob(glob_path)
     
     def anecdotes(dataset: str) -> pd.DataFrame:
         cols = ['text', 'action', 'label', 'binarized_label']
@@ -72,3 +82,25 @@ def scruples(glob_path: str = './data/processed/*scruples*') -> Tuple[pd.DataFra
         df_dilemma.append(d)
 
     return (df_anecdote, df_dilemma)
+
+def moral_stories(glob_path: str = './data/moral_stories_datasets/**/*.jsonl') -> Tuple[pd.DataFrame, pd.DataFrame]:
+    datasets = glob(glob_path)
+    d = read_jsonlines(datasets[0])
+    full_df = pd.DataFrame(d)
+    task, categories = datasets[1].split('/')[3:5]
+    mapped_df = pd.DataFrame(read_jsonlines(datasets[1]), index=None)
+    mapped_df.insert(0, 'Task', task)
+    mapped_df.insert(1, 'Category', categories)
+
+    for dataset in datasets[2:]:
+        task, categories = dataset.split('/')[3:5]
+        tmp = pd.DataFrame(read_jsonlines(dataset), index=None)
+        tmp['Task'] = task
+        tmp['Category'] = categories
+        mapped_df.append(tmp)
+
+    return (full_df, mapped_df)
+
+
+fdf, mdf = moral_stories()
+print(mdf.head())
--- src/pretraining.py
+++ src/pretraining.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from typing import Tuple, Union
 
 import datasets
@@ -7,6 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import jax
+import jax.numpy as jnp
 import haiku as hk
 
 Pipeline = Tuple[transformers.PreTrainedModel, transformers.PreTrainedTokenizer]
@@ -19,7 +21,7 @@ Datatypes = Union[TorchType, NumpyType, JaxType]
 def init_huggingface_models(model: str) -> Pipeline:
     tokenizer = transformers.BertTokenizer.from_pretrained(model)
     model = transformers.BertModel.from_pretrained(model)
-    return model, tokenizer
+    return (model, tokenizer)
 
 
 def forward(x: str, model: transformers.PreTrainedModel, tokenizer: transformers.PreTrainedTokenizer, tensor_type: str):
@@ -33,3 +35,12 @@ def ed():
     data = datasets.load_dataset('empathetic_dialogues')['train']['utterance']
     emb, out = forward(data[0], model, tokenizer, tensor_type='pt')
     return out, emb
+
+
+def test():
+    model, tokenizer = init_huggingface_models('bert-base-uncased')
+    tokenized, out = forward('This is a test string', model, tokenizer, tensor_type='pt')
+    pprint(tokenized)
+    # pprint(out)
+
+test()
