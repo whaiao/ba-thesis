5fd694804254da3f9111e189571a28e182655fcf
Benjamin Beilharz <b@beilharz.io>
2022-01-16 20:48:05 +0100

    preprocessing for inference added

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,3 +1,4 @@
+import pickle
 import re
 from typing import List
 from numpy import ndarray
@@ -13,16 +14,27 @@ from src.utils import read_tsv
 Dataframe = pd.DataFrame
 
 
-def extract_phrases(srl_parses: List[dict]) -> List[str]:
+def extract_phrases(srl_parses: dict) -> List[str]:
     def process(phrase: str) -> str:
-        matches = re.findall(r'\[\w+:[\s\w+]+\]', phrase, re.IGNORECASE)
-        for i in matches:
-            i = re.sub(r'\[\w+:\s', '', i)
-            i = i.replace('[', '')
+        matches = ' '.join(re.findall(r'\[\w+:[\s\w+]+\]', phrase, re.IGNORECASE))
+        matches = re.sub(r'\[\w+:\s', '', matches)
+        matches = matches.replace(']', '')
 
-        return ' '.join(matches)
+        return matches
 
-
+    phrases = []
+    for k, v in srl_parses.items():
+        for parses in v:
+            phrases.append(process(parses.parse))
+        
+    return phrases
+    
 def prepare_overlap(x: str, overlap_mode: str, atomic: Dataframe,
                     dependency_parser, srl_model) -> ndarray:
     sentence = x
+
+if __name__ == "__main__":
+    with open('srl.pickle', 'rb') as p:
+        data = pickle.load(p)
+
+    extract_phrases(data)
