Fixed masking

src/models/dialog_guiding_module/dialog_guiding_module.py
@@ -136,12 +136,14 @@ class DialogGuidingModule(nn.Module):
         Returns:
             knowledge mappings
         """
-        ### EXPERIMENTAL: CAP MAX TOKENS TO 512
-        ntokens = len(query.split())
-        if ntokens > 450:
-            query = ' '.join(query.split()[:450])
+        #### EXPERIMENTAL: CAP MAX TOKENS TO 512
+        #### SOMETHING IS OFF HERE. FIX TO GET VALIDATION TO WORK.
+        ntokens = len(query.split(" "))
+        print(f'{len(query)} -> {ntokens} --- Current Context:{query}')
+        #if ntokens > 450:
+        #    query = ' '.join(query.split(" ")[:450])
 
-        ###
+        ####
 
         overlaps = retrieve_overlap(query)
         if overlaps is None:
@@ -205,7 +207,7 @@ class DialogGuidingModule(nn.Module):
         s = self._knowledge_lookup(string_repr)
         return self._prepare_relations(s)
 
-    def forward(self, x: Tensor, string_repr: str):
+    def forward(self, x: Tensor, string_repr: str, mask: Tensor = None):
         """Forward pass through `DialogGuidingModule`
 
         Args:
@@ -219,7 +221,8 @@ class DialogGuidingModule(nn.Module):
         knowledge = self.knowledge_attention(x,
                                              event=event,
                                              mental=mental,
-                                             moral=moral)
+                                             moral=moral,
+                                             mask=mask)
 
         moral_head = knowledge['moral']
         moral = self._produce_moral_encoding(string_repr,

src/models/dialog_guiding_module/knowledge_transformer.py
@@ -8,7 +8,7 @@ import torch
 from torch import Tensor
 from torch import nn
 from torch.nn import functional as F
-from transformers import AutoModel, T5ForConditionalGeneration, AutoTokenizer
+from transformers import AutoModel, T5EncoderModel, T5ForConditionalGeneration, AutoTokenizer
 
 from src.utils import freeze_weights
 
@@ -22,7 +22,9 @@ class KnowledgeAttention(nn.Module):
                  n_moral_heads: int,
                  dropout: float = .1,
                  hf_checkpoint: str = 'distilbert-base-uncased',
+                 # set false with earlier pretraining model
                  use_pretrained: bool = False,
+                 #use_pretrained: bool = True,
                  share_weights: bool = False,
                  device: torch.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')):
         """Knowledge Attention Module incooperating external knowledge
@@ -52,14 +54,16 @@ class KnowledgeAttention(nn.Module):
         self.tokenizer = AutoTokenizer.from_pretrained(hf_checkpoint)
 
         if use_pretrained:
-            self.encoder = AutoModel.from_pretrained(hf_checkpoint)
+            self.encoder = AutoModel.from_pretrained(hf_checkpoint).to(self.device)
         else:
+            self.tokenizer = AutoTokenizer.from_pretrained('t5-base')
             self.embedding = nn.Embedding(self.tokenizer.vocab_size, embed_dim)
             self.transformer_encoder_layer = nn.TransformerEncoderLayer(
-                embed_dim, nhead=8, batch_first=True)
+                self.d_model, nhead=8, batch_first=True)
             self.encoder = nn.TransformerEncoder(
                 self.transformer_encoder_layer, num_layers=2)
 
+        # ablations: use avg pooling?
         # use the same weights to encode matrices
         if self.share_weights:
             self.linear = nn.Linear(self.d_model, self.d_model * 3)
@@ -100,8 +104,7 @@ class KnowledgeAttention(nn.Module):
         attn_logits = torch.matmul(q, k.transpose(-2, -1))
         attn_logits = attn_logits / sqrt(d_k)
         if mask is not None:
-            # change mask values to make them consistent with other masks
-            attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
+            attn_logits = attn_logits.masked_fill(mask, 0.)
         attention = F.softmax(attn_logits, dim=-1)
         if dropout > 0.0:
             attention = F.dropout(attention, p=dropout)
@@ -110,7 +113,7 @@ class KnowledgeAttention(nn.Module):
 
     def _process_attention_heads(
             self, x: torch.FloatTensor, qkv: torch.FloatTensor, n_heads: int,
-            mask: torch.BoolTensor) -> Tuple[torch.FloatTensor]:
+            mask: torch.BoolTensor = None) -> Tuple[torch.FloatTensor]:
         """Processes matrices to output attention values and weights
 
         Args:
@@ -143,14 +146,14 @@ class KnowledgeAttention(nn.Module):
                 return self.tokenizer(x,
                                       truncation=True,
                                       padding='max_length',
-                                      return_tensors='pt')
+                                      return_tensors='pt').to(self.device)
             else:
                 tokenized = self.tokenizer(x,
                                            truncation=True,
                                            padding='max_length',
                                            return_tensors='pt')
                 emb = self.embedding(tokenized.input_ids.to(self.device))
-                mask = tokenized.attention_mask
+                mask = ~tokenized.attention_mask.to(torch.bool)
 
                 return {'src': emb.to(self.device), 'src_key_padding_mask': mask.to(self.device)}
 
@@ -208,11 +211,11 @@ class KnowledgeAttention(nn.Module):
         context_o, context_attn = self._process_attention_heads(
             context, qkv_context, self.n_context_heads, mask)
         event_o, event_attn = self._process_attention_heads(
-            event, qkv_event, self.n_event_heads, mask)
+            event, qkv_event, self.n_event_heads)
         mental_o, mental_attn = self._process_attention_heads(
-            mental, qkv_mental, self.n_mental_heads, mask)
+            mental, qkv_mental, self.n_mental_heads)
         moral_o, moral_attn = self._process_attention_heads(
-            moral, qkv_moral, self.n_moral_heads, mask)
+            moral, qkv_moral, self.n_moral_heads)
 
         # fix attention mask with regards to different sized input, might as well take attention masks from tokenizer for knowledge
 
@@ -414,8 +417,8 @@ class KnowledgeEncoderBlock(nn.TransformerEncoderLayer):
                                    src_mask, src_key_padding_mask)
             x = x + self._ff_block(self.norm2(x))
         else:
-            x = self.norm1(x + self._sa_block(x, knowledge_attn_head, src_mask,
-                                              src_key_padding_mask))
+            x = self.norm1(x + self._sa_block(x, knowledge_attn_head, attn_mask=src_mask,
+                                              key_padding_mask=src_key_padding_mask))
             x = self.norm2(x + self._ff_block(x))
 
         return x
@@ -444,37 +447,63 @@ class KnowledgeEncoderBlock(nn.TransformerEncoderLayer):
 
 class KnowledgeAttentionEncoder(nn.Module):
     def __init__(self,
-                 encoder_checkpoint: str = 'distilbert-base-uncased',
-                 freeze_encoder: bool = True,
+                 dmodel: int = 768,
+                 encoder_checkpoint: str = 't5-base',
+                 nlayers: int = 2,
+                 freeze_encoder: bool = False,
+                 finetune: bool = False,
+                 # set true if using t5 encoder model
+                 #finetune: bool = True,
                  device: torch.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')):
         super(KnowledgeAttentionEncoder, self).__init__()
         self.device = device
-        self.encoder = AutoModel.from_pretrained(encoder_checkpoint)
+        self.finetune = finetune
+        self.nlayers = nlayers
+        self.tokenizer = AutoTokenizer.from_pretrained(encoder_checkpoint)
+        if finetune:
+            self.encoder = T5EncoderModel.from_pretrained(encoder_checkpoint)
+        else:
+            encoder_layer = nn.TransformerEncoderLayer(d_model=dmodel, 
+                    nhead=8,
+                    batch_first=True, 
+                    activation='gelu')
+            self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=4).to(self.device)
+            self.embedding = nn.Embedding(len(self.tokenizer), dmodel).to(self.device)
         if freeze_encoder:
             freeze_weights(self.encoder)
-        self.tokenizer = AutoTokenizer.from_pretrained(encoder_checkpoint)
         self.encoding_layers = nn.ModuleList(
-            [KnowledgeEncoderBlock(d_model=768, nhead=4) for _ in range(4)])
+            [KnowledgeEncoderBlock(d_model=dmodel, nhead=4) for _ in range(4)])
 
     def forward(self, x: Union[str, Tensor],
                 knowledge_attn_heads: Iterable[Tensor]) -> Tensor:
+        if self.finetune:
+            assert len(knowledge_attn_heads) == len(
+                self.encoding_layers
+            ), 'Number of attention encoding layers does not match number of knowledge attention heads'
+            if isinstance(x, str) or isinstance(x, List[str]):
+                encode = self.tokenizer(x,
+                                        truncation=True,
+                                        padding='max_length',
+                                        return_tensors='pt').to(self.device)
+                # encode = {k: v.to(self.device) for k, v in encode.items()}
+                x = self.encoder(**encode).last_hidden_state
+            elif isinstance(x, torch.FloatTensor):
+                x = self.encoder(inputs_embeds=x).last_hidden_state.to(self.device)
 
-        assert len(knowledge_attn_heads) == len(
-            self.encoding_layers
-        ), 'Number of attention encoding layers does not match number of knowledge attention heads'
-        if isinstance(x, str) or isinstance(x, List[str]):
-            encode = self.tokenizer(x,
-                                    truncation=True,
-                                    padding='max_length',
-                                    return_tensors='pt')
-            encode = {k: v.to(self.device) for k, v in encode.items()}
-            x = self.encoder(**encode).last_hidden_state
-        elif isinstance(x, torch.FloatTensor):
-            x = self.encoder(inputs_embeds=x).last_hidden_state.to(self.device)
-
-        for layer, knowledge_attn_head in zip(self.encoding_layers,
-                                              knowledge_attn_heads):
-            x = layer(src=x, knowledge_attn_head=knowledge_attn_head)
+            for layer, knowledge_attn_head in zip(self.encoding_layers,
+                                                  knowledge_attn_heads):
+                x = layer(src=x, knowledge_attn_head=knowledge_attn_head)
+        else:
+            tokenized = self.tokenizer(x, truncation=True, padding='max_length', return_tensors='pt').to(self.device)
+            input_ids = tokenized.input_ids
+            # 0 for attend, 1 for ignore
+            attention_mask = ~tokenized.attention_mask.to(torch.bool)
+            emb = self.embedding(input_ids)
+            x = self.encoder(emb, src_key_padding_mask=attention_mask)
+            for _ in range(self.nlayers):
+                for layer, knowledge_attn_head in zip(self.encoding_layers, knowledge_attn_heads):
+                    x = layer(src=x, knowledge_attn_head=knowledge_attn_head)
+                            #src_key_padding_mask=attention_mask)
 
         return x
 

src/models/neural_empathy.py
@@ -4,7 +4,7 @@ from pprint import pprint
 import torch
 from torch import Tensor
 from torch import nn
-from transformers import AutoTokenizer, AutoModel, T5ForConditionalGeneration, AutoModelForCausalLM, EncoderDecoderModel
+from transformers import AutoTokenizer, AutoModel, T5ForConditionalGeneration, EncoderDecoderModel, GPT2LMHeadModel
 from transformers.modeling_outputs import Seq2SeqLMOutput
 
 from src.models.dialog_guiding_module.dialog_guiding_module import DialogGuidingModule
@@ -31,6 +31,7 @@ class ModelConfig:
 
     # language model head
     lm_checkpoint: str = 'benjaminbeilharz/t5-conditioned-next-turn'
+    # lm_checkpoint: str = 'benjaminbeilharz/dialoGPT-small-conditioned2nextturn'
     pt_checkpoint: str = 'checkpoints/t5_generator.pt'
     resume_training: bool = True
 
@@ -70,7 +71,6 @@ class NeuralEmpathy(nn.Module):
         self.dialog_transformer.config.num_beams = 4
 
 
-
         self.dialog_guiding_module = DialogGuidingModule(
             d_model=self.cfg.d_model,
             output_dimensions=self.cfg.output_dimensions,
@@ -78,11 +78,16 @@ class NeuralEmpathy(nn.Module):
             hf_checkpoint=self.cfg.hf_checkpoint).to(self.cfg.device)
 
 
+        # create language model head
         if 't5' in self.cfg.lm_checkpoint:
-            self.lm_head = T5ForConditionalGeneration.from_pretrained(self.cfg.lm_checkpoint).to(self.cfg.device)
             self.lm_tokenizer = AutoTokenizer.from_pretrained('t5-base')
-        elif 'DialoGPT' in self.cfg.lm_checkpoint:
-            self.lm_head = AutoModelForCausalLM.from_pretrained('microsoft/DialoGPT-medium').to(self.cfg.device)
+            self.lm_head = T5ForConditionalGeneration.from_pretrained(self.cfg.lm_checkpoint).to(self.cfg.device)
+        elif 'dialoGPT' in self.cfg.lm_checkpoint:
+            special_tokens_dict = {'bos_token': '<BOS>', 'eos_token': '<EOS>', 'pad_token': '<PAD>'}
+            self.lm_tokenizer = AutoTokenizer.from_pretrained('microsoft/DialoGPT-small', model_max_length=512)
+            self.lm_tokenizer.add_special_tokens(special_tokens_dict)
+            self.lm_head = GPT2LMHeadModel.from_pretrained('benjaminbeilharz/dialoGPT-small-conditioned2nextturn').to(self.cfg.device)
+            self.lm_head.resize_token_embeddings(len(self.lm_tokenizer))
         if self.cfg.resume_training:
             pass
 
@@ -108,6 +113,7 @@ class NeuralEmpathy(nn.Module):
                 p.requires_grad = True
 
     def _prepare_lm_input(self, next_turn: str) -> Tensor:
+        next_turn = '<pad> ' + next_turn
         labels = self.lm_tokenizer(next_turn,
                                    padding='longest',
                                    max_length=128,
@@ -127,30 +133,36 @@ class NeuralEmpathy(nn.Module):
         Returns:
             a natural language response
         """
-        if hasattr(self, 'dialog_tokenizer'):
-            tokenized = self.dialog_tokenizer(history, 
-                    turn, 
-                    truncation=True, 
-                    padding='max_length', 
-                    return_tensors='pt').to(self.cfg.device)
+        if hasattr(self, 'dialog_tokenizer') and not isinstance(self.dialog_transformer, EncoderDecoderModel):
+            tokenized = self.dialog_tokenizer(history, turn, truncation=True, padding='max_length', return_tensors='pt').to(self.cfg.device)
             encoded_history = self.dialog_transformer(**tokenized).last_hidden_state
+        # enc-dec model
+        elif isinstance(self.dialog_transformer, EncoderDecoderModel):
+            enc_in = self.dialog_tokenizer(history, truncation=True, padding='max_length', return_tensors='pt').to(self.cfg.device)
+            dec_in = self.dialog_tokenizer(turn, truncation=True, padding='max_length', return_tensors='pt').to(self.cfg.device)
+            encoded_history = self.dialog_transformer(input_ids=enc_in.input_ids,
+                    attention_mask=enc_in.attention_mask,
+                    decoder_input_ids=dec_in.input_ids,
+                    decoder_attention_mask=dec_in.attention_mask,
+                    labels=dec_in.input_ids
+                    ).decoder_hidden_states[0]
         else:
             encoded_history = self.dialog_transformer(history, turn)
+
+        # knowledge attention w/ atomic
         knowledge_encoding = self.dialog_guiding_module(encoded_history, turn)
-        if 't5' in self.cfg.lm_checkpoint:
-            next_utterance = self._prepare_lm_input(next)
-        else:
-            next_utterance = knowledge_encoding
-         
         knowledge_encoding2tokens = torch.argmax(knowledge_encoding, dim=-1)
 
         # if settings are not supplied, use default arguments to generate
         if generation_settings is None:
-            outputs = model.generate(input_ids=knowledge_encoding2tokens)
+            outputs = self.lm_head.generate(input_ids=knowledge_encoding2tokens)
         else:
-            outputs = model.generate(input_ids=knowledge_encoding2tokens, **generation_settings)
+            outputs = self.lm_head.generate(input_ids=knowledge_encoding2tokens, **generation_settings)
+        # now we have 5 output sequences
+        print("Output:\n" + 100 * '-')
+        for i, output in enumerate(outputs):
+            print("{}: {}".format(i, self.lm_tokenizer.decode(output, skip_special_tokens=True)))
 
-        generated = self.lm_tokenizer.decode(outputs[0], skip_special_tokens=True)
         return generated
 
 
@@ -180,12 +192,21 @@ class NeuralEmpathy(nn.Module):
                     ).decoder_hidden_states[0]
         else:
             encoded_history = self.dialog_transformer(history, turn)
-        knowledge_encoding = self.dialog_guiding_module(encoded_history, turn)
+
+        # knowledge attention w/ atomic
+        knowledge_encoding = self.dialog_guiding_module(encoded_history, turn, 
+                # experimental
+                mask=~dec_in.attention_mask.to(torch.bool))
+
+        # language model head - prepare gold label
         if 't5' in self.cfg.lm_checkpoint:
             next_utterance = self._prepare_lm_input(nxt)
+        elif 'dialoGPT' in self.cfg.lm_checkpoint:
+            labels = history + turn
+            next_utterance = self.lm_tokenizer(labels, truncation=True, padding='max_length', return_tensors='pt').to(self.cfg.device).input_ids
         else:
             next_utterance = knowledge_encoding
-         
+
         out = self.lm_head(inputs_embeds=knowledge_encoding,
                            labels=next_utterance)
         return out

src/train.py
@@ -18,7 +18,7 @@ from torch import nn
 from torchmetrics import MetricCollection, BLEUScore
 from torchmetrics.text.bert import BERTScore
 import transformers
-from transformers import AdamW, get_linear_schedule_with_warmup, get_cosine_schedule_with_warmup
+from transformers import Adafactor, AdamW, get_linear_schedule_with_warmup, get_cosine_schedule_with_warmup
 import wandb
 
 from src.models.neural_empathy import NeuralEmpathy, ModelConfig
@@ -29,20 +29,24 @@ from src.utils import init_from_checkpoint
 class TrainingConfig:
     """Training Configuration for `Manager` class"""
     epochs: int = 6
-    learning_rate: float = 1e-4
+    learning_rate: float = 1e-3
     betas: Iterable[float] = None
     gradient_accumulation: bool = True
     report_every: int = 10
     unfreezing_modules: List[str] = field(default_factory=lambda: ['dialog_guiding_module'])
     unfreeze_every: int = 1
-    warmup_steps: int = 500
+    warmup_steps: int = 0
     scheduler: Callable = get_linear_schedule_with_warmup
-    save_to: str = 'checkpoints/models/neural_empath_with_enc_dec'
+    save_to: str = 'checkpoints/models/tdec_fixed'
 
 @dataclass
 class GenerationConfig:
     """Generation Configuration for LM Generation"""
-    num_beams: int = 4
+    num_beams: int = 10
+    max_length: int = 200
+    no_repeat_ngram_size: int = 2
+    num_return_sequences: int = 5
+    early_stopping: bool = True
 
 
 
@@ -70,8 +74,21 @@ class Manager:
             self.optimizer = optimizer
         else:
             self.model = model(model_cfg).to(self.device)
-            self.optimizer = optimizer(self.model.parameters(),
-                     self.cfg.learning_rate)
+            if optimizer.__name__ == 'Adafactor':
+                self.optimizer = optimizer(self.model.parameters(),
+                            lr=1e-3,
+                            eps=(1e-30, 1e-3),
+                            clip_threshold=1.0,
+                            decay_rate=-0.8,
+                            beta1=None,
+                            weight_decay=0.0,
+                            relative_step=False,
+                            scale_parameter=False,
+                            warmup_init=False,
+                            )
+            else:
+                self.optimizer = optimizer(self.model.parameters(),
+                        lr=self.cfg.learning_rate)
 
         self.model.cuda()
         self.data = data
@@ -86,12 +103,6 @@ class Manager:
 
         self.freeze_model_modules = ['dialog_transformer', 'lm_head']
         self.unfreeze_model_modules = self.cfg.unfreezing_modules
-
-        # for module in self.freeze_model_modules:
-        #     for p in getattr(self.model, module).parameters():
-        #         p.requires_grad = False
-
-        # datasets
 
         metrics = MetricCollection([
             BLEUScore(2),
@@ -131,7 +142,20 @@ class Manager:
             mcfg = pickle.load(p)
 
         model = model(cfg)
-        optimizer = optimizer(model.parameters(), cfg.learning_rate)
+        if optimizer.__name__ == 'Adafactor':
+            optimizer = optimizer(model.parameters(),
+                        lr=1e-3,
+                        eps=(1e-30, 1e-3),
+                        clip_threshold=1.0,
+                        decay_rate=-0.8,
+                        beta1=None,
+                        weight_decay=0.0,
+                        relative_step=False,
+                        scale_parameter=False,
+                        warmup_init=False,
+                        )
+        else:
+            optimizer = optimizer(model.parameters(), lr=cfg.learning_rate)
 
         # loading model checkpoint
         model, optimizer = init_from_checkpoint(checkpoint_path, model, optimizer)
@@ -321,13 +345,14 @@ class Manager:
                 print(
                         f'Epoch: {epoch}\tTraining Loss: {np.mean(train_running_loss)}\tPerplexity: {perplexity}\nCurrent Generation: {current_generation}'
                 )
+                if i % 10000 == 0:
+                    self._save_config(self.cfg.save_to)
+                    self._save_checkpoint(self.cfg.save_to)
 
             print(
                 f'Finished Epoch: {epoch}\t Average Training Loss: {np.mean(train_running_loss)}'
             )
             print(f'Saving Model with Average Training Loss {np.mean(train_running_loss)}')
-            self._save_config(self.cfg.save_to)
-            self._save_checkpoint(self.cfg.save_to)
 
             eval_running_loss = []
             for i, sample in enumerate(tqdm(self.data['validation']), start=1):
@@ -358,15 +383,34 @@ class Manager:
 def main():
     cfg = TrainingConfig()
     mcfg = ModelConfig()
+    #gcfg = GenerationConfig().__dict__
+    #print('Config: ', gcfg)
     dataset = load_dataset('benjaminbeilharz/ed-for-lm')
+    #mcfg.lm_checkpoint = 'benjaminbeilharz/t5-conditioned-next-turn'
 
+    # trainer = Manager(cfg, mcfg, NeuralEmpathy, Adafactor, dataset)
     trainer = Manager(cfg, mcfg, NeuralEmpathy, AdamW, dataset)
-    # checkpoint_path = 'checkpoints/models/'
-    # trainer_cfg = checkpoint_path + 'train_cfg_10-03-23'
-    # model_cfg = checkpoint_path + 'model_cfg_10-03-23'
-    # checkpoint = checkpoint_path + 'checkpoint_10-03-23.pt'
-    # trainer = Manager.load_from_config(trainer_cfg, model_cfg, checkpoint, NeuralEmpathy, AdamW, dataset)
+    checkpoint_path = 'checkpoints/models/'
+    # bert knowledge encoder
+    #checkpoint = checkpoint_path + 'adamw-enc-dec-bert-knowledgeencodercheckpoint_20-03-17.pt'
+    #model_cfg = checkpoint_path + 'adamw-enc-dec-bert-knowledgeencodermodel_cfg_20-03-17'
+    #train_cfg = checkpoint_path + 'adamw-enc-dec-bert-knowledgeencodertrain_cfg_20-03-17'
+    # transformer knowledge encoder
+    #checkpoint = checkpoint_path + 'neural_empath_with_enc_deccheckpoint_21-03-07.pt'
+    #model_cfg = checkpoint_path + 'neural_empath_with_enc_decmodel_cfg_21-03-07'
+    #train_cfg = checkpoint_path + 'neural_empath_with_enc_dectrain_cfg_21-03-07'
+
+    #checkpoint = checkpoint_path + 'neural_empath_with_enc_deccheckpoint_24-03-04.pt'
+    #model_cfg = checkpoint_path + 'neural_empath_with_enc_decmodel_cfg_24-03-04'
+    #train_cfg = checkpoint_path + 'neural_empath_with_enc_dectrain_cfg_24-03-04'
+    #checkpoint = checkpoint_path + 'adafactor-enc-deccheckpoint_24-03-09.pt'
+    #model_cfg = checkpoint_path + 'adafactor-enc-decmodel_cfg_24-03-09'
+    #train_cfg = checkpoint_path + 'adafactor-enc-dectrain_cfg_24-03-09'
+    #trainer = Manager.load_from_config(train_cfg, model_cfg, checkpoint, NeuralEmpathy, Adafactor, dataset)
+    #trainer = Manager.load_from_config(train_cfg, model_cfg, checkpoint, NeuralEmpathy, AdamW, dataset)
     trainer.run()
+
+    # trainer.inference_step('I have been in a car accident and now it\'s hard to go to university', 'I feel like the whole world just stopped', **gcfg)
 
 
 main()

src/utils.py
@@ -91,10 +91,10 @@ def init_from_checkpoint(
 ) -> Tuple[torch.nn.Module, torch.optim.Optimizer]:
     model = model
     optim = optimizer
-    checkpoint = torch.load(checkpoint)
+    checkpoint = torch.load(checkpoint, map_location='cpu')
 
-    model.load_state_dict(state_dict=checkpoint['model'])
-    optim.load_state_dict(state_dict=checkpoint['optim'])
+    model.load_state_dict(state_dict=checkpoint['model'], strict=False)
+    # optim.load_state_dict(state_dict=checkpoint['optim'])
     del checkpoint
     torch.cuda.empty_cache()
 