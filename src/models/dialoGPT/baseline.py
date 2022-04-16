1cd0d72ab6f2d9f7cf91253c9cd46786f394017c
Benjamin Beilharz <b@beilharz.io>
2022-04-16 13:15:06 +0200

    baseline eval

--- src/models/dialoGPT/baseline.py
+++ src/models/dialoGPT/baseline.py
@@ -3,7 +3,7 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer, TrainingArguments, Trai
 from numpy import argmax
 import wandb
 
-checkpoint = 'microsoft/dialoGPT-small'
+checkpoint = 'checkpoints/baseline/checkpoint-3715'
 epochs = 5.
 
 data = load_dataset('benjaminbeilharz/empathetic_dialogues_for_lm')
@@ -17,27 +17,40 @@ wandb.init(project='ba-thesis', entity='benjaminbeilharz')
 def tokenize_fn(batch):
     convs = batch['conv']
     dialog = ' '.join([u for u in convs]) + tokenizer.eos_token
-    return tokenizer(dialog, return_tensors='pt', truncation=True,
+    return tokenizer(dialog,
+                     return_tensors='pt',
+                     truncation=True,
                      padding='max_length')
 
+
 def compute_metrics(pred):
-    metric = load_metric('accuracy', 'perplexity')
+    metric_dict = {}
+    perp = load_metric('perplexity')
+    bleu = load_metric('bleu')
+    meteor = load_metric('meteor')
+    bertscore = load_metric('bertscore')
     logits, labels = pred
     predictions = argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
+    for metric in [perp, bleu, meteor, bertscore]:
+        metric_dict.update(
+            metric.compute(predictions=predictions, references=labels))
+
+    return metric_dict
 
 
 data = data.map(tokenize_fn, batched=True).remove_columns('conv')
 
-args = TrainingArguments('checkpoints/dialoGPT-small-baseline',
-                         num_train_epochs=epochs,
-                         save_strategy='epoch',
-                         evaluation_strategy='no',
-                         per_device_train_batch_size=8,
-                         gradient_accumulation_steps=3,
-                         gradient_checkpointing=True,
-                         fp16=True,
-                         report_to='wandb')
+args = TrainingArguments(
+    'checkpoints/baseline',
+    num_train_epochs=epochs,
+    save_strategy='epoch',
+    evaluation_strategy='epoch',
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    gradient_accumulation_steps=3,
+    #gradient_checkpointing=True,
+    fp16=True,
+    report_to='wandb')
 trainer = Trainer(model,
                   args,
                   train_dataset=data['train'],
@@ -47,4 +60,4 @@ trainer = Trainer(model,
                   compute_metrics=compute_metrics)
 
 if __name__ == '__main__':
-    trainer.train()
+    trainer.evaluate()
